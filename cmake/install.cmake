661b16139d0a7729997c9fbfb7c1a91abf63f847
HARMEL, Bernard <b.harmel@evs.com>
2024-03-15 12:24:18 +0100

    delete packaging.cmake

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -45,12 +45,6 @@ endif()
 
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${COMPILED_BINARIES_DIR}/lib)
 
-# == PACKAGING ==
-if (CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
-  # make package only if ${PROJECT_NAME_LOWER_MINUS} is the main project (not a submodule)
-#  include(packaging.cmake)
-endif()
-
 # == POPULATE PROJECTS ==
 string(TOLOWER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_LOWER)
 
